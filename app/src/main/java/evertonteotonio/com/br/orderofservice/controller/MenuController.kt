e18af2fca64f1a3dc67b7a8dad4ba7d4a400bb19
Everton Teotonio <evertontt@gmail.com>
2018-05-29 22:50:01 -0300

    Criando fragmentes

--- app/src/main/java/evertonteotonio/com/br/orderofservice/controller/MenuController.kt
+++ app/src/main/java/evertonteotonio/com/br/orderofservice/controller/MenuController.kt
@@ -3,13 +3,24 @@ package evertonteotonio.com.br.orderofservice.controller
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import evertonteotonio.com.br.orderofservice.R
+import evertonteotonio.com.br.orderofservice.fragment.AboutFragment
 import kotlinx.android.synthetic.main.activity_menu_controller.*
 
 class MenuController : AppCompatActivity() {
 
+    val fragmentMan = supportFragmentManager
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        //Limpando os fragments
+        //val fragmentManager = supportFragmentManager
+        //fragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        clearFragments()
+
         when (item.itemId) {
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
@@ -23,9 +34,12 @@ class MenuController : AppCompatActivity() {
 //                message.setText(R.string.title_about)
 //                return@OnNavigationItemSelectedListener true
 
-                val intent = Intent(this@MenuController, AboutController::class.java)
-                startActivity(intent)
-                this@MenuController.finish()
+                //val intent = Intent(this@MenuController, AboutController::class.java)
+                //startActivity(intent)
+                //this@MenuController.finish()
+
+                //message.setText(R.string.title_about)
+                createFragmentAbout()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -37,5 +51,20 @@ class MenuController : AppCompatActivity() {
         setContentView(R.layout.activity_menu_controller)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    fun createFragmentAbout()
+    {
+        val transaction = fragmentMan.beginTransaction()
+        val fragment = AboutFragment()
+        transaction.replace(R.id.fragmentholder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    fun clearFragments()
+    {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
